38910c82ba6b41d1d28ad068e274a02712a5dbd1
Amina <aminasidorets0204@gmail.com>
2024-04-09 12:17:21 +0300

    lab1

---
+++ lab1/lab1.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function changeText() {
+    document.getElementById("message").innerHTML=("Hello world"); 
+  }
+  
+  function clearText() {
+    document.getElementById("message").innerHTML=("<br>"); 
+  }
+
+
